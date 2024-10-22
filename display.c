da75b2e988d06ccae0a2adb385c69fa68e9e0f2f
AYAYA <95303032+Nox612@users.noreply.github.com>
2024-10-22 16:25:34 +0200

    added main and display

---
+++ display.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "display.h"
+
+int printMap(char *filename)
+{
+    FILE *fp = fopen(filename, "r");
+    
+    if (fp == NULL)
+    {
+        printf("Error: could not open file %s\n", filename);
+        return 1; // Indicate failure
+    }
+    
+    char ch;
+    while ((ch = fgetc(fp)) != EOF)
+    {
+        putchar(ch);
+    }
+    printf("\n");
+    
+    fclose(fp);
+    return 0; // Indicate success
+}
---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "main.h"
+
+int main()
+{
+    char *filename = "maps/map1.md";
+    printMap(filename);
+    return 0;
+}
