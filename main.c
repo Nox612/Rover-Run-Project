ade18623a525cc525da650de8901b31a4732837d
AYAYA <95303032+Nox612@users.noreply.github.com>
2024-10-22 16:36:54 +0200

    reset

--- main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include "main.h"
-
-int main()
-{
-    char *filename = "maps/map1.md";
-    printMap(filename);
-    return 0;
-}
--- main.h
+++
@@ -1,7 +0,0 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-int main();
-int printMap(char *filename);
-
-#endif
